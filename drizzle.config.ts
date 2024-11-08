ae88f7bb524d03aa82eafb35ad4e7076ac739cb1
EC2 Default User <ec2-user@ip-172-31-45-69.eu-central-1.compute.internal>
2024-11-08 23:07:17 +0000

    change db location

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'drizzle-kit'
 export default defineConfig({
   dialect: 'sqlite',
   dbCredentials: {
-    url: 'file:./db.sqlite',
+    url: 'file:data/db.sqlite',
   },
   out: './src/db/migrations',
   schema: './src/db/schema',
--- src/db.ts
+++ src/db.ts
@@ -1,5 +1,5 @@
 import { Database } from 'bun:sqlite'
 import { drizzle } from 'drizzle-orm/bun-sqlite'
 
-const sqlite = new Database('db.sqlite')
+const sqlite = new Database('data/db.sqlite')
 export const db = drizzle({ client: sqlite, casing: 'snake_case' })
