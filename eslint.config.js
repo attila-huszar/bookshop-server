bf294fb3ec40747c204a511038ac9650c25bf4bd
Attila Huszar <attila.huszar@nixs.com>
2024-11-15 18:36:24 +0100

    add  eslint

---
+++ eslint.config.js
@@ -0,0 +1,46 @@
+import eslint from '@eslint/js'
+import tseslint from 'typescript-eslint'
+import globals from 'globals'
+import prettier from 'eslint-plugin-prettier'
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  ...tseslint.configs.recommendedTypeChecked,
+  ...tseslint.configs.stylisticTypeChecked,
+  {
+    files: ['**/*.ts'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      globals: globals.node,
+      parser: tseslint.parser,
+      parserOptions: {
+        projectService: {
+          tsconfigPath: './tsconfig.json',
+        },
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tseslint.plugin,
+      prettier,
+    },
+    rules: {
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        { ignoreRestSiblings: true },
+      ],
+      '@typescript-eslint/consistent-type-definitions': 'off',
+      'prettier/prettier': 'warn',
+    },
+    linterOptions: {
+      reportUnusedDisableDirectives: 'warn',
+    },
+  },
+  {
+    ignores: ['node_modules'],
+  },
+  {
+    files: ['**/*.js'],
+    ...tseslint.configs.disableTypeChecked,
+  },
+)
