3e34ebfa86def43ef864e4c4dcf7545da9af0104
Attila Huszar <attila.huszar@nixs.com>
2024-11-16 21:16:36 +0100

    add stripe to env config

--- src/config/envConfig.ts
+++ src/config/envConfig.ts
@@ -39,3 +39,5 @@ export const jwtRefreshExpiration = getEnvVar(
 
 export const ngrokAuthToken = getEnvVar('NGROK_AUTHTOKEN', null)
 export const ngrokDomain = getEnvVar('NGROK_DOMAIN', null)
+
+export const stripeSecret = getEnvVar('STRIPE_SECRET')
