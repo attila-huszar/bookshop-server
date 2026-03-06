165e1bd106ac253855d0c19245744b8707fe0f88
Attila Huszar <attila.huszar@outlook.com>
2026-03-06 14:58:21 +0100

    fix: correct command syntax in run_with_cronitor.sh for cronitor execution

--- docker/cron/jobs/run_with_cronitor.sh
+++ docker/cron/jobs/run_with_cronitor.sh
@@ -6,7 +6,7 @@ MONITOR_KEY="$1"
 shift
 
 if [ -n "$CRONITOR_API_KEY" ]; then
-  exec cronitor --env "${NODE_ENV:-production}" exec "$MONITOR_KEY" "$@"
+  exec cronitor exec --env "${NODE_ENV:-production}" "$MONITOR_KEY" "$@"
 fi
 
 exec "$@"
